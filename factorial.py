fab42777fb6c7bc3cd0360e3816e1d14d39b7ebd
shteypandey28-hue <shteypandey28@gmail.com>
2025-10-26 06:33:12 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,9 @@
+#Write a function factorial(n) that returns the factorial of a number
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
